f954965af2d94d371279b020f69a24f33ba89ba7
Joswin Prince <inf@improveminds.in>
2023-12-18 12:39:52 +0530

    seperating client[vite] and [express] server application's

---
+++ client/src/components/MyComponent.jsx
@@ -0,0 +1,8 @@
+// MyComponent.jsx
+import React from 'react';
+
+const MyComponent = () => {
+  return <div>Hello, JSX!</div>;
+};
+
+export default MyComponent;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // server/server.js
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
