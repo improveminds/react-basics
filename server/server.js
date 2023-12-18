17454c32e6a928abe80b6773e3a83c76aab17e88
Joswin Prince <inf@improveminds.in>
2023-12-18 21:57:15 +0530

    msql dependencies added for express js server ! npm command's updated for the application

--- server/server.js
+++ server/server.js
@@ -1,11 +1,49 @@
 // server/server.js
 
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'installation_lifecycle_manager'
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+//get user's
+app.get('/project', (req, res) => {
+  // Example SQL query to retrieve users
+  const sql = 'SELECT * FROM project';
+
+  // Execute the query
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express.js!' });
+});
+
+app.get('/test', (req, res) => {
+  res.send('Testing route!');
 });
 
 app.listen(port, () => {
